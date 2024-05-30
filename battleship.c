776b7218b6ee76fc7da332e769dafee0873256c3
wonwoong <wonwoong71@kau.ac.kr>
2024-05-30 20:17:02 +0900

    modify code and comment

--- battleship.c
+++ battleship.c
@@ -4,7 +4,7 @@
 #define BOARD_SIZE 5 // 게임 보드의 크기 정의
 #define SHIPS_COUNT 4 // 게임의 전함 수 정의
 
-// 플레이어 정보 구조체 정의  
+// 플레이어 정보 구조체 정의
 typedef struct {
     char name[50];
     int score;
@@ -12,7 +12,6 @@ typedef struct {
 
 
 void singleplay();
-
 void multplay();
 void gamemode();
 void initializeBoard(char board[][BOARD_SIZE]);
@@ -26,17 +25,15 @@ int read_scores(Player players[], int max_players);
 void print_sorted_scores(Player players[], int count);
 void showRanking();
 
-
-
-
 int main() {
      //게임 모드 설정
     gamemode();
 
     return 0;
 }
-void singleplay(){ //싱글플레이 함수 
-    char board[BOARD_SIZE][BOARD_SIZE];
+
+void singleplay(){
+     char board[BOARD_SIZE][BOARD_SIZE];
     int guessRow, guessCol;
     int attempts = 0;
     int a=0;
@@ -45,9 +42,9 @@ void singleplay(){ //싱글플레이 함수
     placeShips(board); //배치  -> 랜덤 배치 들어오면 추가해줌.
 
     // 게임 설명 출력
-    printf("=== 전함 게임 ===\n");
-    printf("보드 상의 전함 위치를 추측하세요.\n");
-    printf("0부터 4까지의 행과 열 번호를 입력하세요.\n");
+    printf("=== Battleship Game ===\n");
+    printf("Guess the location of the battleship on the board.\n");
+    printf("Enter row and col numbers from 0 to 4.\n");
 
     // 게임 루프
     while (!hasWon(board)) {  //보드에 전함이 남아있으면
@@ -56,24 +53,24 @@ void singleplay(){ //싱글플레이 함수
         printBoard(board); //군함을 감춰서 출력력
 
         // 사용자의 다음 수를 입력받음
-        printf("추측할 좌표를 입력하세요 (행 열): ");
-        scanf("%d %d", &guessRow, &guessCol);  //사용자가 추측하는 군함의 위치치
+        printf("Enter the coordinates you want to guess (row col): ");
+        scanf("%d %d", &guessRow, &guessCol);  //사용자가 추측하는 군함의 위치
 
         // 사용자의 추측을 유효성 검사함
         if (!isValidGuess(guessRow, guessCol)) { //사이즈가 벗어나면
-            printf("잘못된 추측입니다! 유효한 좌표를 입력하세요.\n");
-            continue; //다시 while문의 처음으로 감감
+            printf("This is a wrong guess! Please enter valid coordinates.\n");
+            continue; //다시 while문의 처음으로 감
         }
 
         // 추측이 전함을 맞췄는지, 실패했는지, 이전에 이미 추측한 위치인지 확인함
         if (board[guessRow][guessCol] == 'S') {
-            printf("축하합니다! 전함을 격추했습니다!\n");
+            printf("Congratulations! The battleship has been shot down!\n");
             board[guessRow][guessCol] = 'X';  // 'X'는 맞힌 곳을 나타냄
         } else if (board[guessRow][guessCol] == '~') {
-            printf("실패했습니다! 다시 시도하세요.\n");
+            printf("Failed! Please try again.\n");
             board[guessRow][guessCol] = 'O';  // 'O'는 실패한 곳을 나타냄
         } else {
-            printf("이미 추측한 위치입니다. 다시 시도하세요.\n");
+            printf("The location has already been guessed. Please try again.\n");
         }
 
         attempts++; //시도 횟수 증가
@@ -82,25 +79,20 @@ void singleplay(){ //싱글플레이 함수
     // 게임 종료, 최종 보드 상태와 시도 횟수 출력
     printf("\n");
     printBoard(board);
-    printf("축하합니다! 모든 전함을 격추했습니다. 시도 횟수: %d\n", attempts);
+    printf("congratulations! All battleships have been shot down. attempts: %d\n", attempts);
     
     rank_input(attempts);
-    printf("랭킹표를 확인하시겠습니까 ? 1: O 2: X ");
+    printf("Would you like to check the ranking table? 1: O else: X ");
     scanf("%d",&a);
     if(a==1){
         showRanking();
-    }
-    else if(a==2){
-        return;
     }
     else{
         return;
     } 
 
 }
-
-
-void multplay(){ //멀티플레이 함수 
+void multplay(){
     char board1[BOARD_SIZE][BOARD_SIZE]; //1플레이어 보드
     char board2[BOARD_SIZE][BOARD_SIZE]; //2플레이어 보드 
     
@@ -114,9 +106,9 @@ void multplay(){ //멀티플레이 함수
     placeShips(board2);
 
     // 게임 설명 출력
-    printf("=== 전함 게임 ===\n");
-    printf("보드 상의 전함 위치를 추측하세요.\n");
-    printf("0부터 4까지의 행과 열 번호를 입력하세요.\n");
+    printf("=== Battleship Game ===\n");
+    printf("Guess the location of the battleship on the board.\n");
+    printf("Enter row and col numbers from 0 to 4.\n");
 
     // 게임 루프
     while (!hasWon(board1) && !hasWon(board2)) {  //보드에 전함이 남아있으면
@@ -129,45 +121,45 @@ void multplay(){ //멀티플레이 함수
         
 first_player:
         // 사용자의 다음 수를 입력받음
-        printf("1플레이어: 추측할 좌표를 입력하세요 (행 열): ");
-        scanf("%d %d", &guessRow1, &guessCol1);  //사용자가 추측하는 군함의 위치치
+        printf("Player 1: Enter the coordinates you want to guess (row col): ");
+        scanf("%d %d", &guessRow1, &guessCol1);  //사용자가 추측하는 군함의 위치
         if (!isValidGuess(guessRow1, guessCol1)) { //사이즈가 벗어나면
-            printf("잘못된 추측입니다! 유효한 좌표를 입력하세요.\n");
-            goto first_player; //다시 while문의 처음으로 감감
+            printf("This is a wrong guess! Please enter valid coordinates.\n");
+            goto first_player; //first_player: 이 부분으로 가서 플레이어 1의 입력을 다시 받음 
         }
 second_player:
-        printf("2플레이어: 추측할 좌표를 입력하세요 (행 열): ");
+        printf("Player 2: Enter the coordinates you want to guess (row col): ");
         scanf("%d %d", &guessRow2, &guessCol2);
         // 사용자의 추측을 유효성 검사함
-        if (!isValidGuess(guessRow2, guessCol2)) { //사이즈가 벗어나면   //수정필요요
-            printf("잘못된 추측입니다! 유효한 좌표를 입력하세요.\n");
-            goto second_player; //다시 while문의 처음으로 감감
+        if (!isValidGuess(guessRow2, guessCol2)) { //사이즈가 벗어나면 
+            printf("This is a wrong guess! Please enter valid coordinates.\n");
+            goto second_player; //second_player: 이부분으로 가서 플레이어 2의 입력을 다시 받음
         }
 
         // 추측이 전함을 맞췄는지, 실패했는지, 이전에 이미 추측한 위치인지 확인함
         if (board1[guessRow1][guessCol1] == 'S') {
-            printf("1p: 축하합니다! 전함을 격추했습니다!\n");
+            printf("1p: Congratulations! The battleship has been shot down!\n");
             board1[guessRow1][guessCol1] = 'X';  // 'X'는 맞힌 곳을 나타냄
         } else if (board1[guessRow1][guessCol1] == '~') {
-            printf("1p: 실패했습니다! 다시 시도하세요.\n");
+            printf("1p: Failed! Please try again.\n");
             board1[guessRow1][guessCol1] = 'O';  // 'O'는 실패한 곳을 나타냄
         } else {
-            printf("1p: 이미 추측한 위치입니다. 다시 시도하세요.\n");
+            printf("1p: The location has already been guessed. Please try again.\n");
         }
         if (board2[guessRow2][guessCol2] == 'S') {
-            printf("2p: 축하합니다! 전함을 격추했습니다!\n");
+            printf("2p: Congratulations! The battleship has been shot down!\n");
             board2[guessRow2][guessCol2] = 'X';  // 'X'는 맞힌 곳을 나타냄
         } else if (board2[guessRow1][guessCol1] == '~') {
-            printf("2p: 실패했습니다! 다시 시도하세요.\n");
+            printf("2p: Failed! Please try again.\n");
             board2[guessRow2][guessCol2] = 'O';  // 'O'는 실패한 곳을 나타냄
         } else {
-            printf("2p: 이미 추측한 위치입니다. 다시 시도하세요.\n");
+            printf("2p: The location has already been guessed. Please try again.\n");
         }
 
         
     }
 
-    // 게임 종료, 최종 보드 상태와 시도 횟수 출력
+    // 게임 종료, 최종 보드 상태출력 
     printf("\n");
     printf("\n");
     printBoard(board1);
@@ -176,13 +168,13 @@ second_player:
     
     
     if(hasWon(board1) && hasWon(board2)){
-        printf("무승부 입니다");
+        printf("It's a draw");
     }
     if(hasWon(board1) && !hasWon(board2)){
-        printf("p1 승리 ! 축하합니다! 모든 전함을 격추했습니다.");
+        printf("p1 wins! congratulations! All battleships have been shot down.");
     }
     if(!hasWon(board1) && hasWon(board2)){
-        printf("p2 승리 ! 축하합니다! 모든 전함을 격추했습니다.");
+        printf("p2 wins! congratulations! All battleships have been shot down.");
     }
 
 }
@@ -190,8 +182,8 @@ second_player:
 //싱글 플레이와 멀티 플레이를 선택하는 함수
 void gamemode(){
     int n;
-    printf("모드를 선택하세요 \n");
-    printf("1: 싱글모드 2:멀티모드 ");
+    printf("Select Mode \n");
+    printf("1: SiglePlay 2:MultiPlay ");
     scanf("%d",&n);
     if(n==1){
         singleplay();
@@ -200,8 +192,8 @@ void gamemode(){
         multplay();
     }
     else{
-        printf("잘못 입력하셨습니다.\n");
-        gamemode();
+        printf("wrong!\n");
+        gamemode(); //잘못입력시 다시 모드 선택하도록 함.
     }
     
 }
@@ -236,18 +228,18 @@ void placeShips(char board[][BOARD_SIZE]) {
     int i;
     for (i = 0; i < SHIPS_COUNT; i++) { //현재는 4개를 입력받음
         int row, col;  //행,열 
-        printf("전함 %d의 좌표를 입력하세요 (행 열): ", i + 1); //전함 1 2 3 4의 좌표 입력 -> 출력에 (i+1)과 같은 형태는 좋지 않음 (개선 필요)
-        scanf("%d %d", &row, &col); // 행, 열 입력 받음음
+        printf("Enter the coordinates of the battleship%d (row col): ", i + 1);
+        scanf("%d %d", &row, &col); // 행, 열 입력 받음
 
-        if (row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE) { //보드 사이즈 안에 있는 행과 열이면면
+        if (row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE) { //보드 사이즈 안에 있는 행과 열이면
             if (board[row][col] != '~') { //이미 전함이 있으면 
-                printf("해당 위치에 이미 전함이 있습니다. 다시 입력하세요.\n");
+                printf("There is already a battleship at that location. Please enter again.\n");
                 i--;
             } else { 
                 board[row][col] = 'S';  // 'S'는 전함을 나타냄
             }
         } else {  // 보드 사이즈를 넘어서면 
-            printf("잘못된 좌표입니다. 다시 입력하세요.\n");
+            printf("Invalid coordinates. Please enter again.\n");
             i--;
         }
     }
@@ -270,17 +262,18 @@ int hasWon(char board[][BOARD_SIZE]) {
     }
     return 1;  // 모든 전함이 격추됨
 }
+
 // 플레이어 정보 입력 함수
 void rank_input(int score) {
     char player_name[50];
 
-    printf("게임오버 이름입력: ");
+    printf("Game Over Enter name: ");
     scanf("%s", player_name);
 
     // 메모장 열기
     FILE* file = fopen("Rank_list.txt", "a");
     if (file == NULL) {
-        printf("파일 열기 오류\n");
+        printf("File open error\n");
         return;
     }
 
@@ -294,12 +287,12 @@ void rank_input(int score) {
 int read_scores(Player players[], int max_players) {
     FILE* file = fopen("Rank_list.txt", "r");
     if (file == NULL) {
-        printf("파일 열기 오류\n");
+        printf("File open error\n");
         return 0;
     }
 
     int count = 0;
-    while (fscanf(file, "Player Name: %49s\tScore: %d\n", players[count].name, &players[count].score) == 2) {
+    while (fscanf(file, "Player Name: %49s\tScore: %d\n", players[count].name, &players[count].score) == 2) { //fscanf의 반환값이 2이면  2가지 값 모두 잘 가져왔단 의미 
         count++;
         if (count >= max_players) {
             break;
@@ -310,11 +303,11 @@ int read_scores(Player players[], int max_players) {
     return count;
 }
 
-// 점수를 기준으로 내림차순 정렬하는 함수
+// 점수를 기준으로 오름차순 정렬하는 함수
 int compare_scores(const void* a, const void* b) {
     Player* playerA = (Player*)a;
     Player* playerB = (Player*)b;
-    return playerA->score - playerB->score; // 내림차순 정렬
+    return playerA->score - playerB->score; // 오름차순 정렬
 }
 
 // 정렬된 결과 출력 함수
@@ -326,8 +319,7 @@ void print_sorted_scores(Player players[], int count) {
 }
 
 void showRanking() {
-    // 입력받기 (필요시 주석 해제)
-    // rank_input();
+    
 
     // 최대 플레이어 수 정의
     const int MAX_PLAYERS = 100;
@@ -342,6 +334,4 @@ void showRanking() {
     // 정렬된 결과 출력
     print_sorted_scores(players, player_count);
 
-   
 }
-
